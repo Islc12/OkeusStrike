65c47d86c8a978d5c1d51872cc97cecc470928c5
Rich Smith <richrsmith@proton.me>
2025-03-04 15:26:06 -0500

    Added setup for deauth and downgrade attack scripts. Will still have to go back and write these. Also deleted those functions from the main script. We'll instead write separate scripts and call those into the main one

---
+++ Addfuncts/deauth.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+def deauth():
+    # initialize a deauth attack
+    pass
---
+++ Addfuncts/downgrade.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+def downgrade():
+    # initialize a downgrade attack
+    pass
